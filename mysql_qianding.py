ddc22bf39481d9708c81f0badc983bb64bc35661
18301102@bjtu.edu.cn <18301102@bjtu.edu.cn>
2020-12-13 15:16:25 +0800

    审批合同

--- mysql_qianding.py
+++ mysql_qianding.py
@@ -31,12 +31,12 @@ def database_yiqianding(connection, cursor):
     return result
 
 
-# 审批合同
+# 签订合同
 def database_huiqian(connection, cursor, name, userName, qianding):
     lock.acquire()
     timenum = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
     # 更新数据
-    sql = "UPDATE contract_process SET content='" + qianding + "',state='1',time='" + timenum + "' WHERE contract_process.con_id=(SELECT id FROM contract WHERE name='" + name + "') and contract_process.use_id=(SELECT id FROM user WHERE name='" + userName + "') and contract_process.type=2"
+    sql = "UPDATE contract_process SET content='" + qianding + "',state='1',time='" + timenum + "' WHERE contract_process.con_id=(SELECT id FROM contract WHERE name='" + name + "') and contract_process.use_id=(SELECT id FROM user WHERE name='" + userName + "') and contract_process.type=3"
     cursor.execute(sql)
     # 提交数据
     connection.commit()
