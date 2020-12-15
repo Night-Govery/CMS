68c8bd9f73e2d49c4f9507677f0847bc2cd5f1f2
Night_Govery <208989395@qq.com>
2020-12-15 19:53:25 +0800

    修改了log页面

--- mysql_log.py
+++ mysql_log.py
@@ -5,7 +5,7 @@ from cffi.cparser import lock
 def database_loglist(connection, cursor, userName):
     lock.acquire()
     # 执行数据查询
-    sql = "select log.id AS use_id,user.name AS userName,log.content AS logContent, log.time AS logTime from user,log"
+    sql = "select log.id AS log_id,user.name AS userName,log.content AS logContent, log.time AS logTime from user,log"
     cursor.execute(sql)
     # 获取数据库单条数据
     result = cursor.fetchall()
