bc4d673afa40df7ce65b6acbcfc513b7016a0b11
18301102@bjtu.edu.cn <18301102@bjtu.edu.cn>
2020-12-15 18:08:55 +0800

    编辑、删除客户

--- mysql_customer.py
+++ mysql_customer.py
@@ -39,8 +39,11 @@ def database_addcustomer(connection, cursor, customerName, customerAddress, cust
 def database_deletecustomer(connection, cursor, customerName, userName):
     lock.acquire()
     #删除未完成合同
-    sql = "DELETE FROM customer WHERE id=(SELECT id FROM customer WHERE name='" + customerName + "')"
-    # 删除
+    sql = "DELETE FROM contract WHERE cus_id=(SELECT id FROM customer WHERE name='" + customerName + "')"
+    cursor.execute(sql)
+    # 提交数据
+    connection.commit()
+    # 删除客户
     sql = "DELETE FROM customer WHERE id=(SELECT id FROM customer WHERE name='" + customerName + "')"
     cursor.execute(sql)
     # 提交数据
@@ -49,5 +52,12 @@ def database_deletecustomer(connection, cursor, customerName, userName):
     return None
 
 
-def database_editcustomer(connection, cursor, address, phone, fax, code, bankname, bankaccount):
+def database_editcustomer(connection, cursor, customerName, address, phone, fax, code, bankname, bankaccount):
+    lock.acquire()
+    #修改信息
+    sql = "UPDATE customer SET name='" + customerName + "',address='" + address + "',tel='" + phone + "',fax='" + fax + "',code='" + code + "',bank='" + bankname + "',account='" + bankaccount + "' WHERE id=(SELECT id FROM customer WHERE name='" + customerName + "')"
+    cursor.execute(sql)
+    # 提交数据
+    connection.commit()
+    lock.release()
     return None
