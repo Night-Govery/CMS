925b1f4f678d7c2d7ce34b0645ae6d0f666f821c
18301102@bjtu.edu.cn <18301102@bjtu.edu.cn>
2020-12-16 10:49:18 +0800

    增加了查看会签意见和查看审批意见两个方法

--- mysql_dinggao.py
+++ mysql_dinggao.py
@@ -64,3 +64,31 @@ def database_dinggao(connection, cursor, name, userName, dinggao):
         connection.commit()
         lock.release()
         return 1
+
+
+# 查看所有会签意见
+def database_huiqianyijian(connection, cursor, name, userName):
+    lock.acquire()
+    # 执行数据查询
+    sql = "SELECT DISTINCT contract_process.content AS huiqiancontent from contract,contract_process" \
+          " where contract_process.con_id=contract.id and contract_process.type=1 and contract.id=(SELECT id FROM contract WHERE name ='" + name + "')"
+    cursor.execute(sql)
+    # 获取数据库单条数据
+    result = cursor.fetchall()
+    connection.commit()
+    lock.release()
+    return result
+
+
+# 查看所有审批意见
+def database_shenpiyijian(connection, cursor, name, userName):
+    lock.acquire()
+    # 执行数据查询
+    sql = "SELECT DISTINCT contract_process.content AS shenpicontent from contract,contract_process" \
+          " where contract_process.con_id=contract.id and contract_process.type=2 and contract.id=(SELECT id FROM contract WHERE name ='" + name + "')"
+    cursor.execute(sql)
+    # 获取数据库单条数据
+    result = cursor.fetchall()
+    connection.commit()
+    lock.release()
+    return result
